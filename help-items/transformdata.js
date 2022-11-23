e3d5cd90f52bd61ddfa146bfb371b1a52231b383
Kevin Miller <kemiller2002@gmail.com>
2022-11-22 21:50:33 -0500

    add mockup and data

---
+++ help-items/transformdata.js
@@ -0,0 +1,47 @@
+const data = `OAK
+AllowCreditCardPayments
+Shows/Hides Credit Card payments
+FALSE
+OAK
+CreditCardGateway
+Picks the credit card transaction handler
+Authorize.net
+OAK
+CreditCardTestMode
+Will use Gateway Test/Demo mode if applicable
+FALSE
+OAK
+CreditCardLogin
+API Login ID for Authorize.net / Vendor for Paypal
+OAK
+CreditCardPrivateKey
+Not always used - Authorize.Net uses this.
+OAK
+CreditCardSharedKey
+Not always used - Stripe needs this.
+OAK
+CreditCardUserAccount
+Not always used - Paypal uses though
+OAK
+CreditCardPassword
+Not always used - Paypal uses though
+OAK
+CreditCardFeePercent
+Percent to calculate off amount
+0
+OAK
+CreditCardFeeFixed
+Fixed amount of charge
+0
+OAK
+CreditCardsAccepted
+Type of cards accepted, will allow basic validation of card numbers.
+visa, mastercard
+OAK
+CreditCardContributionDisclaimer
+Text specific to credit card contributions that should display when choosing that option
+HTML Content
+OAK
+CreditCardRequireBillingAddress
+Adds/Removes validation requiring user to fill out billing address
+FALSE`;
